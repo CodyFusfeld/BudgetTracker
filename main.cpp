af97b808777d4cee4cdaed94b80b3276c8dbd868
CodyFusfeld <cjfusfeld@gmail.com>
2025-05-21 16:18:11 -0700

    Working on addTransaction functionality

--- main.cpp
+++ main.cpp
@@ -1,10 +1,72 @@
 #include <iostream>
+#include <string>
+
+
+void ProgramGreeting() {
+    std::cout << "Welcome to the Budget Tracker" << std::endl;
+    std::cout << "Developer by Cody Fusfeld" << std::endl;
+    std::cout << std::endl;
+}
 
 
 
 
 int main() {
+    ProgramGreeting();
+
+    int choice;
+    bool SwitchLoop = true;
+
+    while (SwitchLoop) {
+        std::cout << "Main Menu" << std::endl;
+        std::cout << "Add Transaction" << std::endl;
+        std::cout << "View Transaction" << std::endl;
+        std::cout << "Exit Program" << std::endl;
+        std::cout << "Enter a choice(1-3) " << std::endl;
+        std::cin >> choice;
+
+    switch(choice){
+        case 1:
+           //addTransaction();
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+
+
+        default:
+            std::cout << "Invalid Choice, Please Select (1-3): " << std::endl;
+
+
+    }
+
+        
+    }
     
+
+
+
+    switch(choice){
+        case 1:
+            
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+
+
+
+        default:
+            std::cout << "Invalid Choice, Please Select (1-3): " << std::endl;
+
+
+    }
 
 
 
---
+++ utils.h
@@ -0,0 +1,10 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include "Transaction.h"
+#include <vector>
+
+
+
+
+#endif
